63c4ff0ff770ab223b1d2b0db32af4d433bfcc4f
Peter Askey <aspe@summacollege.nl>
2023-11-07 11:11:18 +0100

    error handling

--- src/Controller/MazePuzzleController.php
+++ src/Controller/MazePuzzleController.php
@@ -4,6 +4,10 @@ namespace App\Controller;
 
 use App\Repository\MazePuzzleRepository;
 use Doctrine\DBAL\ConnectionException;
+use Doctrine\DBAL\Exception\InvalidFieldNameException;
+use Doctrine\DBAL\Exception\SyntaxErrorException;
+use Doctrine\DBAL\Exception\TableNotFoundException;
+use Doctrine\DBAL\Query\QueryException;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,13 +20,22 @@ final class MazePuzzleController extends AbstractController
     public function mazepuzzle(Request $request, EntityManagerInterface $entityManager, MazePuzzleRepository $mazePuzzleRepository): Response
     {
         if ($request->isMethod("POST")) {
+            /**
+             * Database user for Entity manager connection only has read access so any other type of query will
+             * throw a ConnectionException
+             */
+
+            $query = $request->get('query');
             try{
-                $puzzleRows = $entityManager->getConnection()->executeQuery($request->get('query'))->fetchAllAssociative();
-                return $this->render('maze.html.twig', ['puzzleRows' => $puzzleRows]);
+                $puzzleRows = $entityManager->getConnection()->executeQuery($query)->fetchAllAssociative();
+                return $this->render('maze.html.twig', ['puzzleRows' => $puzzleRows , 'query' => $request->get('query')]);
 
             }
             catch (ConnectionException $e){
                 return $this->render('error.html.twig');
+            }
+            catch (QueryException|TableNotFoundException|InvalidFieldNameException|SyntaxErrorException $e){
+                return $this->render('maze.html.twig', ['puzzleRows' => [], 'queryException' => $e->getMessage(), 'query' => $request->get('query')]);
             }
         }
 
