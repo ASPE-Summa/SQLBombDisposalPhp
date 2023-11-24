72fc52f6005064ec0747adcc6261e82d93a90f4f
Peter Askey <aspe@summacollege.nl>
2023-11-24 10:34:06 +0100

    finished adventurer puzzle

---
+++ src/Controller/AdventurerController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\MazePuzzleRepository;
+use Doctrine\DBAL\ConnectionException;
+use Doctrine\DBAL\Exception\InvalidFieldNameException;
+use Doctrine\DBAL\Exception\SyntaxErrorException;
+use Doctrine\DBAL\Exception\TableNotFoundException;
+use Doctrine\DBAL\Query\QueryException;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class AdventurerController extends AbstractController
+{
+    #[Route('/adventurerpuzzle')]
+    public function buttonPuzzle(Request $request, EntityManagerInterface $userEntityManager): Response
+    {
+        if ($request->isMethod("POST")) {
+            /**
+             * Database user for Entity manager connection only has read access so any other type of query will
+             * throw a ConnectionException
+             */
+
+            $query = $request->get('query');
+            try{
+                $puzzleRows = $userEntityManager->getConnection()->executeQuery($query)->fetchAllAssociative();
+                return $this->render('adventurers.html.twig', ['puzzleRows' => $puzzleRows , 'query' => $request->get('query')]);
+
+            }
+            catch (ConnectionException $e){
+                return $this->render('error.html.twig');
+            }
+            catch (QueryException|TableNotFoundException|InvalidFieldNameException|SyntaxErrorException $e){
+                return $this->render('adventurers.html.twig', ['puzzleRows' => [], 'queryException' => $e->getMessage(), 'query' => $request->get('query')]);
+            }
+        }
+
+        $puzzleRows = $userEntityManager->getConnection()->executeQuery("SELECT * FROM adventurer")->fetchAllAssociative();
+        return $this->render('adventurers.html.twig', ['puzzleRows' => $puzzleRows]);
+    }
+}
